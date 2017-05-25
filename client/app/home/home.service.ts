824b8db844d5914ada7d8daebd7b5f3b81a3ca14
Mohammed9531 <mohd.shoukath7866@gmail.com>
2017-05-25 01:00:42 -0500

    (feat) Added reusable service & moved the test code to home component.

---
+++ client/app/home/home.service.ts
@@ -0,0 +1,40 @@
+import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers } from '@angular/http';
+
+const headers: Headers  = new Headers();
+headers.append('Content-Type', 'application/json');
+
+@Injectable()
+export class HomeService {
+    private _url: any = 'userslist';
+
+    constructor(private http: Http) {}
+
+    createUser(_user: object): Observable<any> {
+        return this.http
+        .post(this._url, {data: _user}, {
+            headers: headers
+        })
+        .map(res => res.json());
+    }
+
+    deleteUser(_id: string): Observable<any>  {
+        const _url = this._url + '/' + _id;
+        return this.http
+        .delete(_url)
+        .map(res => res.json());
+    }
+
+    editUser(_id: string, _user: object): Observable<any>  {
+        const _url = this._url + '/' + _id;
+        return this.http
+        .put(_url, {
+           data: _user
+        }, {
+           headers: headers
+        })
+        .map(res => res.json());
+    }
+}
